4fdaefdaf2fd875d513acb0cc1a3a931562a9bc7
hybelius <hybelius@student.chalmers.se>
2020-06-17 09:36:36 +0200

    Fixed bug only saving half correction chain

--- analysis.py
+++ analysis.py
@@ -28,10 +28,8 @@ def main(file_path, RL_args, prediction_args):
     for i in tqdm(range(reader.get_capacity())):
         tmp_dict = {}
         qubit_matrix, eq_distr = reader.next()
-        print(qubit_matrix)
 
-        success, correction_list = rl.prediction_mod(**prediction_args, qubit_matrix=qubit_matrix) # Anpassa efter Alexeis kod
-        drl_correction_chain = correction_list[-1] # last value in correction_list is complete correction chain
+        success, drl_correction_chain = rl.prediction_mod(**prediction_args, qubit_matrix=qubit_matrix) # Anpassa efter Alexeis kod
 
         drl_smart = (define_equivalence_class(drl_correction_chain) == np.argmax(eq_distr))
         if success and not drl_smart:
@@ -40,7 +38,8 @@ def main(file_path, RL_args, prediction_args):
             data.append(tmp_dict)
         
     df = pd.DataFrame(data)
-    df.to_pickle('drl_failures.xz')
+    p_str = file_path[file_path.find('p_0'):file_path.find('.xz')]
+    df.to_pickle('drl_failures_{}.xz'.format(p_str))
 
 
 # Returns number of degenerate equivalence classes
@@ -56,8 +55,8 @@ def count_degenerate(eq_distr, rel_tol=0.1):
 
 if __name__ == '__main__':
     size = 5
-    p_error = 0.1
-    file_path = './data/data_5x5_p_0.2.xz'
+    p_error = 0.15
+    file_path = './data/data_5x5_p_0.15.xz'
     prediction_args = {'prediction_list_p_error': [0.0], 'PATH': './network/Size_5_NN_11.pt'}
     RL_args = {'Network': NN_11, 'Network_name': 'Size_5_NN_11', 'system_size': size, 
         'p_error': p_error, 'replay_memory_capacity': 1e4, 'DATA_FILE_PATH': file_path}
